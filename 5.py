d42e663a1cd195434746f2358ea917d95bb80ad3
jackstrosahl <strosahljack@gmail.com>
2022-12-06 13:47:32 -0800

    5 part 2 done

--- 5.py
+++ 5.py
@@ -1,6 +1,9 @@
 from collections import deque
+from copy import deepcopy
 from re import split
 
+def stacks_string(stacks):
+    return "".join(stack[-1] if stack else " " for stack in stacks)
 
 with open("5.txt") as f:
     lines = [line.rstrip("\n") for line in f]
@@ -22,10 +25,21 @@ for crate_line in crate_lines:
             continue
         stacks[i].appendleft(crate)
 
+stacks_p1 = deepcopy(stacks)
 moves = [tuple(int(num) for num in split(r"[^0-9]+",move_line)[1:]) for move_line in move_lines]
 
 for count, source, dest in moves:
     for _ in range(count):
-        stacks[dest - 1].append(stacks[source - 1].pop())
+        stacks_p1[dest - 1].append(stacks_p1[source - 1].pop())
 
-print("".join(stack[-1] for stack in stacks))
+
+print(stacks_string(stacks_p1))
+
+stacks_p2 = deepcopy(stacks)
+for count, source, dest in moves:
+    moving = deque()
+    for _ in range(count):
+        moving.appendleft(stacks_p2[source - 1].pop())
+    stacks_p2[dest - 1].extend(moving)
+
+print(stacks_string(stacks_p2))
