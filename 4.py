064cf07beea98ed6e2cd0f0fe9e3fc78a84705f0
jackstrosahl <strosahljack@gmail.com>
2022-12-06 09:31:10 -0800

    4 done

--- 4.py
+++ 4.py
@@ -1,5 +1,15 @@
-with open("4-test.txt") as f:
+with open("4.txt") as f:
     pairs = [[[int(i) for i in section_range.split("-")] for section_range in line.rstrip("\n").split(",")] for line in f]
 
+ans_p1 = 0
+ans_p2 = 0
 for pair in pairs:
-    sections = [set(range()) for section_range in pair]
+    sections = [set(range(start, end+1)) for start,end in pair]
+    common = sections[0].intersection(sections[1])
+    if common in sections:
+        ans_p1 += 1
+    if common:
+        ans_p2 += 1
+
+print(ans_p1)
+print(ans_p2)
