6b3b0e17415abd66d51917afd6a793d4f190825d
jackstrosahl <strosahljack@gmail.com>
2022-12-06 14:15:36 -0800

    6 part 2 done

--- 6.py
+++ 6.py
@@ -3,16 +3,20 @@ from re import search
 def marker_regex(n):
     regex = ""
     for i in range(n):
-        regex += r"(.)"
         if i > 0:
             regex += r"(?!"
             regex += r"|".join(fr"\{j}" for j in range(1,i+1))
             regex += r")"
-
+        regex += r"(.)"
     return regex
 
-with open("6-test.txt") as f:
+with open("6.txt") as f:
     lines = [line.rstrip("\n") for line in f]
 
 for line in lines:
-    print(search(r"(.)(?!\1)(.)(?!\1|\2)(.)(?!\1|\2|\3)(.)", line).end())
+    print(search(marker_regex(4), line).end())
+
+print()
+
+for line in lines:
+    print(search(marker_regex(14), line).end())
