bd420c7a36c5f2eaefb19584141b6a39ebeb6dca
jackstrosahl <strosahljack@gmail.com>
2022-12-06 14:47:36 -0800

    1 part 2 done

--- 1.py
+++ 1.py
@@ -1,15 +1,31 @@
+from collections import deque
+from sortedcontainers import SortedList
+
+
 with open("1.txt") as f:
-    lines = f.readlines()
+    lines = [line.rstrip("\n") for line in f]
+    lines.append("")
 
 elf = 0
-best = 0
+elves = []
 for line in lines:
-    if line == "\n":
+    if line == "":
+        elves.append(elf)
         elf = 0
         continue
     elf += int(line)
-    if elf > best:
-        best = elf
-        
 
-print(best)
+print(max(elves))
+
+top = SortedList()
+for elf in elves:
+    if len(top) < 3:
+        top.add(elf)
+        continue
+    for other in reversed(top):
+        if elf > other:
+            top.pop(0)
+            top.add(elf)
+            break
+
+print(sum(top))
