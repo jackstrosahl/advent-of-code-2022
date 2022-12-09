14bea7a22a15d65b875aa0a2917c56c4d5f294cd
jackstrosahl <strosahljack@gmail.com>
2022-12-09 10:17:51 -0800

    9 part 2 done

--- 9.py
+++ 9.py
@@ -16,26 +16,31 @@ movements = {
     "D": (0,-1)
 }
 
-head = (0,0)
-tail = (0,0)
+def get_visited(length):
+    rope = [(0,0) for _ in range(length)]
 
-visited = set()
+    visited = set()
 
-for direction, distance in moves:
-    movement = movements[direction]
-    for i in range(distance):
-        head = add_tuples(head,movement)
-        diff_x, diff_y = (sub(*x) for x in zip(head,tail))
-        tail_x, tail_y = tail
-        if abs(diff_x) > 1 or abs(diff_y) > 1:
-            if diff_y == 0:
-                tail_x += sign(diff_x)
-            elif diff_x == 0:
-                tail_y += sign(diff_y)
-            else:
-                tail_x += sign(diff_x)
-                tail_y += sign(diff_y)
-        tail = (tail_x,tail_y)
-        visited.add(tail)
+    for direction, distance in moves:
+        movement = movements[direction]
+        for i in range(distance):
+            rope[0] = add_tuples(rope[0],movement)
+            for i in range(1,length):
+                prev_knot, cur_knot = rope[i-1:i+1]
+                diff_x, diff_y = (sub(*x) for x in zip(prev_knot,cur_knot))
+                tail_x, tail_y = cur_knot
+                if abs(diff_x) > 1 or abs(diff_y) > 1:
+                    if diff_y == 0:
+                        tail_x += sign(diff_x)
+                    elif diff_x == 0:
+                        tail_y += sign(diff_y)
+                    else:
+                        tail_x += sign(diff_x)
+                        tail_y += sign(diff_y)
+                rope[i] = (tail_x,tail_y)
+            visited.add(rope[-1])
+    
+    return len(visited)
 
-print(len(visited))
+print(get_visited(2))
+print(get_visited(10))
