bcf60d5b926c85535553e8fed3cca039ca61a412
strojac <strosahljack@gmail.com>
2022-12-10 16:43:45 -0800

    10 part 2 done

--- 10.py
+++ 10.py
@@ -3,7 +3,8 @@ with open("10.txt") as f:
 
 x = 1
 clock = 1
-ans = 0
+ans_p1 = 0
+ans_p2 = ""
 
 for instruction in instructions:
     op = instruction[0]
@@ -15,8 +16,14 @@ for instruction in instructions:
         value = int(instruction[1])
     for _ in range(length):
         if clock <= 220 and (clock-20) % 40 == 0:
-            ans += clock * x
+            ans_p1 += clock * x
+        if abs(x-((clock-1)%40)) <= 1:
+            ans_p2 += "#"
+        else:
+            ans_p2 += "."
         clock += 1
     x += value
 
-print(ans)
+print(ans_p1)
+for i in range(0,len(ans_p2),40):
+    print(ans_p2[i:i+40])
