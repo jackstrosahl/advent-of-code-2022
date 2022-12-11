2c60a5b2ca2ff75a217f71a2b7978fd10bf34bed
strojac <strosahljack@gmail.com>
2022-12-11 15:21:08 -0800

    11 part 2 slow

--- 11.py
+++ 11.py
@@ -1,11 +1,13 @@
 from collections import Counter, deque
+from copy import deepcopy
 from functools import reduce
+from math import log
 from operator import add, mul, sub, truediv
 
 
 cur_monkey = []
 cur_test = []
-monkeys = []
+monkeys_start = []
 
 op_strs = {
     "+": add,
@@ -14,7 +16,7 @@ op_strs = {
     "*": mul,
 }
 
-with open("11.txt") as f:
+with open("11-test.txt") as f:
     for monkey_i, line in enumerate(f):
         line = line.strip()
         match monkey_i%7:
@@ -29,6 +31,7 @@ with open("11.txt") as f:
                 try:
                     val = int(val)
                 except ValueError:
+                    assert op is mul
                     op = pow
                     val = 2
                 cur_monkey.append((op,val))
@@ -43,20 +46,30 @@ with open("11.txt") as f:
                 cur_test.append(int(line))
                 cur_monkey.append(cur_test)
                 cur_test = []
-                monkeys.append(cur_monkey)
-                cur_monkey = []     
+                monkeys_start.append(cur_monkey)
+                cur_monkey = []
 
-activity = Counter()
-for round in range(20):
-    for monkey_i, (items, (op, val), (test_div, true_monkey, false_monkey)) in enumerate(monkeys):
-        for item in items:
-            activity[monkey_i] += 1
-            item = op(item,val) // 3
-            if item % test_div == 0:
-                target_monkey = true_monkey
-            else:
-                target_monkey = false_monkey
-            monkeys[target_monkey][0].append(item)
-        items.clear()
 
-print(reduce(mul, (times for monkey, times in activity.most_common(2))))
+def get_activity(rounds,relief):
+    monkeys = deepcopy(monkeys_start)
+    activity = Counter()
+    for round in range(rounds):
+        if round % 100 == 0:
+            print(round)
+        for monkey_i, (items, (op, val), (test_div, true_monkey, false_monkey)) in enumerate(monkeys):
+            for item in items:
+                activity[monkey_i] += 1
+                item = op(item,val)
+                if relief:
+                    item //= 3
+                if item % test_div == 0:
+                    target_monkey = true_monkey
+                else:
+                    target_monkey = false_monkey
+                monkeys[target_monkey][0].append(item)
+            items.clear()
+    
+    return reduce(mul, (times for monkey, times in activity.most_common(2)))
+
+print(get_activity(20,True))
+print(get_activity(20,False))
